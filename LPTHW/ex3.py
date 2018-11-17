024556388a359ae1140c2abff7243a263f4ceb45
ikjotebhasin <ikjoteb@gmail.com>
2018-11-16 20:14:05 -0500

    Completed ex0-3, created file for ex4.

---
+++ LPTHW/ex3.py
@@ -0,0 +1,33 @@
+# Prints the string
+print("I will now count my chickens:")
+
+# Prints the string and then a number after on same line
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# Prints the string
+print("Now I will count the eggs:")
+
+# Prints the value of the operation
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Prints the string
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+# Prints the valur of the operation
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Prints the string and then a number after on same line
+print("What is 3.0 + 2.0?", 3.0 + 2.0)
+print("Whay is 5.0 - 7.0?", 5.0 - 7.0)
+
+# Prints the string
+print("Oh, that's why it's False.")
+
+# Prints the string
+print("How about some more.")
+
+# Prints the string and then a number after on same line
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
